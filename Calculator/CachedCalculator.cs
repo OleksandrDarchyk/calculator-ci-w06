7f67ce13eb3c2241ae6a0f90d6e8bd5120462ae2
OleksandrDarchyk <oledar01@easv365.dk>
2026-02-18 20:07:26 +0100

    fix sonar qube error

--- Calculator/CachedCalculator.cs
+++ Calculator/CachedCalculator.cs
@@ -4,63 +4,43 @@ namespace Calculator;
 
 public class CachedCalculator : ICalculator
 {
-    public readonly SimpleCalculator _calculator = new();
-    public readonly Dictionary<string, Calculation> _cache = new();
-    
+    private readonly SimpleCalculator _calculator = new();
+    private readonly Dictionary<string, Calculation> _cache = new();
+
+    public int CacheCount => _cache.Count;
+
     public int Add(int a, int b)
-    {
-        var calc = GetCachedResult<int>(a, b) ?? StoreInCache(_calculator.Add(a, b), a, b);
-        return calc.Result;
-    }
+        => (GetCachedResult<int>(a, b) ?? StoreInCache(_calculator.Add(a, b), a, b)).Result;
 
     public int Subtract(int a, int b)
-    {        
-        var calc = GetCachedResult<int>(a, b) ?? StoreInCache(_calculator.Subtract(a, b), a, b);
-        return calc.Result;
-    }
+        => (GetCachedResult<int>(a, b) ?? StoreInCache(_calculator.Subtract(a, b), a, b)).Result;
 
     public int Multiply(int a, int b)
-    {        
-        var calc = GetCachedResult<int>(a, b) ?? StoreInCache(_calculator.Multiply(a, b), a, b);
-        return calc.Result;
-    }
+        => (GetCachedResult<int>(a, b) ?? StoreInCache(_calculator.Multiply(a, b), a, b)).Result;
 
     public int Divide(int a, int b)
-    {        
-        var calc = GetCachedResult<int>(a, b) ?? StoreInCache(_calculator.Divide(a, b), a, b);
-        return calc.Result;
-    }
+        => (GetCachedResult<int>(a, b) ?? StoreInCache(_calculator.Divide(a, b), a, b)).Result;
 
     public int Factorial(int n)
-    {
-        var calc = GetCachedResult<int>(n) ?? StoreInCache(_calculator.Factorial(n), n);
-        return calc.Result;
-    }
+        => (GetCachedResult<int>(n) ?? StoreInCache(_calculator.Factorial(n), n)).Result;
 
     public bool IsPrime(int candidate)
-    {
-        var calc = GetCachedResult<bool>(candidate) ?? StoreInCache(_calculator.IsPrime(candidate), candidate);
-        return calc.Result;
-    }
-    
-    private Calculation<T>? GetCachedResult<T>(int a, int? b = null, [CallerMemberName]string operation = "")
+        => (GetCachedResult<bool>(candidate) ?? StoreInCache(_calculator.IsPrime(candidate), candidate)).Result;
+
+    private Calculation<T>? GetCachedResult<T>(int a, int? b = null, [CallerMemberName] string operation = "")
     {
         var calc = new Calculation<T>(default, operation, a, b);
-        if (_cache.ContainsKey(calc.GetKey()))
-        {
-            return (Calculation<T>?)_cache[calc.GetKey()];
-        }
-        return null;
+        var key = calc.GetKey();
+        return _cache.TryGetValue(key, out var value) ? (Calculation<T>?)value : null;
     }
-    
-    private Calculation<T> StoreInCache<T>(T result, int a, int? b = null, [CallerMemberName]string operation = "")
+
+    private Calculation<T> StoreInCache<T>(T result, int a, int? b = null, [CallerMemberName] string operation = "")
     {
         var calc = new Calculation<T>(result, operation, a, b);
-        calc.Result = result;
         _cache.Add(calc.GetKey(), calc);
         return calc;
     }
-    
+
     public class Calculation
     {
         private int A { get; }
@@ -73,14 +53,11 @@ public class CachedCalculator : ICalculator
             B = b;
             Operation = operation;
         }
-    
-        public string GetKey()
-        {
-            return string.Concat(A, Operation, B);
-        }
+
+        public string GetKey() => string.Concat(A, Operation, B);
     }
 
-    private class Calculation<T>(T? result, string operation, int a, int? b = null)
+    private sealed class Calculation<T>(T? result, string operation, int a, int? b = null)
         : Calculation(operation, a, b)
     {
         public T? Result { get; set; } = result;
